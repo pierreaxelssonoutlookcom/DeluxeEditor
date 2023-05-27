efdaa64bcc04266b907da5dec96d9d82aa84152c
Pierre Axelsson <pierre.axelsson@outlook.com>
2023-05-27 22:15:34 +0200

    Added config file and plugin paths

--- DeluxeEdit.Model/PluginManager.cs
+++ DeluxeEdit.Model/PluginManager.cs
@@ -3,21 +3,36 @@ using DeluxeEdit.Model;
 using System.Reflection;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace DeluxeEdit.Model
 {
     //todo:will now instead u se nuget as plugins
     public class PluginManager
     {
+        private string myPluginPath;
+        private string futurePluginPath;
+
+        public PluginManager()
+        {
+            //note: path is temporary 
+            myPluginPath = $"{new DirectoryInfo(Assembly.GetEntryAssembly().Location).FullName}\\bin";
+            futurePluginPath = $"{Environment.SpecialFolder.ApplicationData}\\DeluxeEdit\\plugins";
+
+        }
         private static Assembly? loadedAsm=null;
+  
+        public void LoadDefaultPlugins()
+        {
 
 
+        }      
         public void ShowPluginManager()
         {
         }
         public List<INamedActionPlugin> LoadPlugins(string path)
         {
-            //done:could be multiple plugisn in the same, FILE
+            //done:could be multiple plugisAssemblyn in the same, FILE
             loadedAsm = Assembly.LoadFile(path);
             var result = new List<INamedActionPlugin>();
                                     
