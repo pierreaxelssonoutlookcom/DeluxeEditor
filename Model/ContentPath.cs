2e6629f7657992825c8f180a262c8700c32a67cf
Pierre Axelsson <pierre.axelsson@outlook.com>
2024-03-22 11:47:18 +0100

    Implemented IEquatable

--- Model/ContentPath.cs
+++ Model/ContentPath.cs
@@ -1,9 +1,18 @@
-﻿namespace Model
+﻿using System;
+
+namespace Model
 {
-  public class ContentPath
+  public class ContentPath: IEquatable<ContentPath>
     {
         public string Path { get; set; }
         public string Content { get; set; }
         public string Header { get; set; }
+
+
+        public bool Equals(ContentPath other)
+        {
+            bool result = other != null ? other.Path == Path && other.Content == Content && other.Header == Header : false;
+            return result;
+        }
     }
 }
